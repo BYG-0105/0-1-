b48d649a462885a9cd364047e3ca1786deba6cbd
BYG-0105 <201971010259@nwnu.edu.cn>
2022-03-22 01:26:23 +0800

    细节修改

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
    public static void main(String[] args)
    {
 	     Frame frame = new Frame();
-         frame.setTitle("Student");//定义名称
+         frame.setTitle("0-1背包问题");//定义名称
          frame.setBounds(40,40, 700, 600);//定义位置及大小
          frame.setVisible(true);
  		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//关闭
